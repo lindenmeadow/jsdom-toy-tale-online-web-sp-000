6384af42fee4ab6a550d19e6dbf20b7b6bbb3690
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-06 21:10:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
